da011aa94c8c27f6c2a59a2aed96e0c9a16e8c73
Vadzim Zieńka <v@vadzim.info>
2019-05-12 11:29:37 +0300

    adopted

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,9 @@
+# 0.0.0 (2019-05-12)
+
+
+### Features
+
+* Add standard-changelog ([79a3336](https://github.com/vadzim/MODULE/commit/79a3336))
+
+
 
---
+++ src/sayHello.js
@@ -0,0 +1,8 @@
+// @flow
+
+/**
+ * This function says hello.
+ * @param name Some name to say hello for.
+ * @returns The hello.
+ */
+export const sayHello = (name: string = "Haz"): string => `Hello, ${name}!`
